15a839bd296d8e6cd5acbf6491b492cadd773d83
Atrion <Atrion@gmail.com>
2018-11-13 00:29:57 -0400

    repo and wizard fixed

--- plugin.program.AtrionWizard/uservar.py
+++ plugin.program.AtrionWizard/uservar.py
@@ -1,28 +1,38 @@
-import os, xbmc
+import os, xbmc, xbmcaddon
 
 #########################################################
 ### User Edit Variables #################################
 #########################################################
-ADDON_ID       = 'plugin.program.AtrionWizard'
-ADDONTITLE     = 'Atrion Wizard'
-EXCLUDES       = [ADDON_ID , 'repository.Atrion17']
-BUILDFILE      = 'http://macarthurhouse.ddns.net/Kodi_Build/txt/builds.txt'                     # Text File with build info in it.
+ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
+ADDONTITLE     = 'Atrions Wizard'
+EXCLUDES       = [ADDON_ID]
+# Text File with build info in it.
+BUILDFILE      = 'http://macarthurhouse.ddns.net/Kodi_Build/txt/builds.txt'
 # How often you would list it to check for build updates in days
 # 0 being every startup of kodi
-UPDATECHECK    = 0                                                                        
+UPDATECHECK    = 0
+# Text File with apk info in it.
+APKFILE        = 'http://'
+# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
+YOUTUBETITLE   = ''
+YOUTUBEFILE    = 'http://'
+# Text File for addon installer.  Leave as 'http://' to ignore
+ADDONFILE      = 'http://'
+# Text File for advanced settings.  Leave as 'http://' to ignore
+ADVANCEDFILE   = 'http://'
 
 # Dont need to edit just here for icons stored locally
-HOME           = xbmc.translatePath('special://home/')
-PLUGIN         = os.path.join(HOME,     'addons',    ADDON_ID)                                   
-ART            = os.path.join(PLUGIN,   'resources', 'art')
+PATH           = xbmcaddon.Addon().getAddonInfo('path')
+ART            = os.path.join(PATH, 'resources', 'art')
 
 #########################################################
 ### THEMING MENU ITEMS ##################################
 #########################################################
-# If you want to use locally stored icons the place them in the Resources/Art/ 
+# If you want to use locally stored icons the place them in the Resources/Art/
 # folder of the wizard then use os.path.join(ART, 'imagename.png')
+# do not place quotes around os.path.join
 # Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
-#          ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
+#           ICONSETTINGS  = 'http://macarthurhouse.ddns.net/Kodi_Build/img/settings.png'
 # Leave as http:// for default icon
 ICONBUILDS     = 'http://macarthurhouse.ddns.net/Kodi_Build/img/builds.png'
 ICONMAINT      = 'http://macarthurhouse.ddns.net/Kodi_Build/img/maintenance.png'
@@ -35,36 +45,70 @@ ICONREAL       = 'http://macarthurhouse.ddns.net/Kodi_Build/img/realdebrid.png'
 ICONLOGIN      = 'http://macarthurhouse.ddns.net/Kodi_Build/img/login.png'
 ICONCONTACT    = 'http://macarthurhouse.ddns.net/Kodi_Build/img/contact.png'
 ICONSETTINGS   = 'http://macarthurhouse.ddns.net/Kodi_Build/img/settings.png'
-HIDESPACERS    = 'No'                                                                    # Hide the ====== seperators 'Yes' or 'No'                           
+# Hide the ====== seperators 'Yes' or 'No'
+HIDESPACERS    = 'No'
+# Character used in seperator
+SPACER         = '='
 
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
 COLOR1         = 'dodgerblue'
 COLOR2         = 'white'
-THEME1         = '[COLOR '+COLOR1+'][Aftermath][/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'    # Primary menu items   / %s is the menu item and is required
-THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'                                          # Build Names          / %s is the menu item and is required
-THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'                                          # Alternate items      / %s is the menu item and is required
-THEME4         = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]' # Current Build Header / %s is the menu item and is required
-THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]' # Current Theme Header / %s is the menu item and is required
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Atrions Wizard[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
 
 # Message for Contact Page
-HIDECONTACT    = 'Yes'                                                                    # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
+# Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
+HIDECONTACT    = 'Yes'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing Atrion Wizard.'
+CONTACT        = ''
+#Images used for the contact window.  http:// for default icon and fanart
+CONTACTICON    = 'http://'
+CONTACTFANART  = 'http://'
 #########################################################
 
 #########################################################
 ### AUTO UPDATE #########################################
 ########## FOR THOSE WITH NO REPO #######################
-AUTOUPDATE     = 'No'                                                                    # Enable Auto Update 'Yes' or 'No'
+# Enable Auto Update 'Yes' or 'No'
+AUTOUPDATE     = 'No'
+# Url to wizard version
 WIZARDFILE     = 'http://macarthurhouse.ddns.net/Kodi_Build/txt/builds.txt'                          # Url to wizard version
+#########################################################
+
+#########################################################
+### AUTO INSTALL ########################################
+########## REPO IF NOT INSTALLED ########################
+# Enable Auto Install 'Yes' or 'No'
+AUTOINSTALL    = 'No'
+# Addon ID for the repository
+REPOID         = ''
+# Url to Addons.xml file in your repo folder(this is so we can get the latest version)
+REPOADDONXML   = ''
+# Url to folder zip is located in
+REPOZIPURL     = ''
 #########################################################
 
 #########################################################
 ### NOTIFICATION WINDOW##################################
 #########################################################
-NOTIFICATION   = ''                                                                      # Url to notification file
-ENABLE         = 'No'                                                                    # Enable Notification screen Yes or No
-FONTSETTINGS   = 'Font12'                                                                # Font for Notification Window
-BACKGROUND     = ''                                                                      # Background for Notification Window
+# Enable Notification screen Yes or No
+ENABLE         = 'No'
+# Url to notification file
+NOTIFICATION   = ''
+# Use either 'Text' or 'Image'
+HEADERTYPE     = ''
+HEADERMESSAGE  = ''
+# url to image if using Image 424x180
+HEADERIMAGE    = ''
+# Background for Notification Window
+BACKGROUND     = ''
 #########################################################
